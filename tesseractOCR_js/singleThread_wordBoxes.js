c101f2edfaca303075f2201aad08736fd1c43637
lp970703 <1277838445@qq.com>
2023-04-24 18:10:19 +0800

    实现opencv对文字位置进行画框

--- tesseractOCR_js/singleThread_wordBoxes.js
+++ tesseractOCR_js/singleThread_wordBoxes.js
@@ -11,6 +11,7 @@ const { createWorker } = require('tesseract.js');
 const path = require('path');
 const moment = require('moment');
 const cv = require('@u4/opencv4nodejs');
+const { Rect, Point2 } = require('opencv4nodejs');
 
 /**
  * 1、createWorker：起一个线程worker去处理OCR识别
@@ -53,18 +54,34 @@ const tesseractOCR = async ({targetPhotoDir, languages}) => {
 
 /**
  * 3、利用opencv将字符标定
- * @param {*} param0 
+ * @param {*} param 参数
+ * @param {*} param.targetPhotoDir 要识别的路径地址
+ * @param {*} param.wordsPosition 图片中文字位置
+ * @param {*} param.outputPhotoDir 将画框的图片输出的路径
+ * 
  */
-const handlePhoto = async ({targetPhotoDir, wordsPosition}) => {
+const handlePhoto = async ({targetPhotoDir, wordsPosition, outputPhotoDir}) => {
+
   // 1、读取img图像
+  // cv.imread(targetPhotoDir)返回了一个Mat对象，下面的cvtColor()、drawRectangle()等方法都是Mat对象下的方法
   let img = cv.imread(targetPhotoDir);
+
   // 2、将cv读取的默认格式BGR改为跟tesseract一样的RGB格式
-  img = cv.cvtColor(img, cv.COLOR_BGR2BGR555);
-  // let param = img.shape();
-  for (const item of wordsPosition) {
-    cv.rectangle(img, (item[1],item[2]), (item[3],item[4]), (0,0,255), 2)
+  // img = cv.cvtColor(img, cv.COLOR_BGR2BGR555);   //python写法
+  img.cvtColor(cv.COLOR_BGR2BGR555);
+
+  // 3、遍历将wordsPosition的文字框出来
+  for (const item of wordsPosition) { 
+
+    // 这里面Rect(x: number, y: number, width: number, height: number)对象四个参数分别指的是：
+    // x -> 左上角原点，从左往右依次增大的像素位置；  y -> 左上角为原点，从上到下依次增大的像素位置； width -> 以x,y为原点，再往右偏移为正值的坐标值；  height -> 以x,y为原点，再往下偏移为正值的坐标值；
+    // new cv.Rect(item[1], item[2], item[3]-item[1], item[4]-item[2]) === new cv.Point2(item[1], item[2]), new cv.Point2(item[3], item[4]) // 两种写法，都表示框住的坐标
+    img.drawRectangle(new cv.Rect(item[1], item[2], item[3]-item[1], item[4]-item[2]), new cv.Vec3(0, 255, 0), 1, cv.LINE_4);
+
+    // cv.rectangle(img, (item[1],item[2]), (item[3],item[4]), (0,0,255), 2)  //python写法
+    cv.imwrite(outputPhotoDir, img);
   }
-  cv.imshow("a", img);
+  // cv.waitKey()监听键盘事件的方法，参数是多少ms（毫秒）
   cv.waitKey(0);
 }
 
@@ -88,12 +105,27 @@ const outPosition = async ({targetPhotoDir, languages}) => {
   return wordsPosition;
 }
 
-const main = async ({targetPhotoDir, languages}) => {
+
+/**
+ * 4、main：主函数
+ * @param {*} param 
+ * @param {*} param.targetPhotoDir 要识别的路径地址
+ * @param {*} param.languages 图片中存在的语言
+ * @param {*} param.outputPhotoDir 将画框的图片输出的路径
+ * 
+ */
+const main = async ({targetPhotoDir, languages, outputPhotoDir}) => {
+  // 1、输出文字位置
   const wordsPosition = await outPosition({targetPhotoDir, languages});
-  await handlePhoto({targetPhotoDir, wordsPosition});
+  // 2、通过文字位置，用opencv将文字位置用方框标出
+  await handlePhoto({targetPhotoDir, wordsPosition, outputPhotoDir});
 }
 
+
+
+// 方法调用
 main({
   targetPhotoDir: path.join(__dirname, './images/taobaoShop1.png'),
-  languages: 'chi_sim+eng'
+  languages: 'chi_sim+eng',
+  outputPhotoDir: path.join(__dirname, './output/singleThread_wordBoxes/taobaoShop1Res.png')
 })
